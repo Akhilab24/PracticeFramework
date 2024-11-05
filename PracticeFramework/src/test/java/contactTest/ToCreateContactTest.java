1d3617b2318bb3cc2659a2c3152b0e2256b26f91
hp <hp@DESKTOP-R4B0E5V>
2024-11-05 08:50:20 +0530

    Practice code

---
+++ PracticeFramework/src/test/java/contactTest/ToCreateContactTest.java
@@ -0,0 +1,12 @@
+package contactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Succesfully added the contact test script to github");
+
+	}
+
+}
---
+++ PracticeFramework/src/test/java/organizationTest/ToCreateOrganizationTest.java
@@ -0,0 +1,11 @@
+package organizationTest;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Succesfully added the organization test scipt to github");
+
+	}
+
+}
